9890d891007b409aa68373db51d8f6316a2c124d
Mischa11111 <132138714+Mischa11111@users.noreply.github.com>
2024-05-20 19:19:31 +0200

    Update disp_IP.py

--- disp_IP.py
+++ disp_IP.py
@@ -42,7 +42,13 @@ try:
 
 	# Draw a black filled box to clear the image.
 	draw.rectangle((0,0,width,height), outline=0, fill=0)
-
+	
+	# Draw some shapes.
+	# First define some constants to allow easy resizing of shapes.
+	padding = -2
+	top = padding
+	bottom = height-padding
+	
 	# Move left to right keeping track of the current x position for drawing shapes.
 	x = 0
 
